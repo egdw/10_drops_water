4b1b23b4f8548b00de310c57ce7e1c9a4ad6d033
Fangluobin <q1217830193@163.com>
2018-07-25 11:37:29 +0800

    爆炸算法简单写出来了思路可能还要换一下没来的及注释

--- js/game.js
+++ js/game.js
@@ -5,83 +5,70 @@ app.controller('gameCtrl',function($scope){
     $scope.clicktime=0;
     $scope.level=1;
     $scope.gameArr=[
-        [2,1,3,2,2,3],
-        [3,4,3,2,0,2],
-        [0,1,4,0,2,1],
-        [2,0,0,1,1,0],
-        [2,0,3,1,3,1],
-        [2,0,4,3,3,4]
+        [2,1,3,2,2,0],
+        [4,4,4,3,0,0],
+        [3,1,2,2,2,0],
+        [0,2,2,0,3,1],
+        [2,4,1,3,1,3],
+        [3,0,3,2,3,4]
     ];
     $scope.clickConfig=function(x,y){
         $scope.clicktime++;
         $scope.dots--;
-        $scope.gameArr[x][y]++;
-        if($scope.gameArr[x][y]===5)
-        {
-            dotblast($scope.gameArr,x,y);
-            $scope.gameArr[x][y]=0;
-        }
-        if($scope.dots===0) {
-            alert("you died!");
-        }
+        dotclick($scope.gameArr,x,y);
+        console.log($scope.gameArr);
     }
 });
 
-function dotblast(arr,x,y){
-    console.log('local:('+x+','+y+')');
-    let tempArr=[x-1,y-1,x+1,y+1];//先向四个方向分别进一步
-    for(let i=1;i<6;i++){
-        for(let j=0;j<4;j++){//遍历四个方向
-            if(tempArr[j]>=0&&tempArr[j]<=5){//如果某个方向超出范围那么直接跳过
-                switch (j){
-                    case 0:tempArr[j]=x-i;break;
-                    case 1:tempArr[j]=y-i;break;
-                    case 2:tempArr[j]=x+i;break;
-                    case 3:tempArr[j]=y+i;break;
-                }
-            }
-        }
-        console.log(tempArr);
-        console.log('up:('+tempArr[0]+','+y+')');
-        console.log('left:('+x+','+tempArr[1]+')');
-        console.log('down:('+tempArr[2]+','+y+')');
-        console.log('right:('+x+','+tempArr[3]+')');
-        for(j=0;j<4;j++){//遍历四个方向
-            if(tempArr[j]>=0 && tempArr[j]<=5){
-                switch(j){
-                    case 0:
-                        if(arr[tempArr[j]][y]!==0){
-                            console.log('arr['+tempArr[j]+']['+y+']='+arr[tempArr[j]][y]);
-                            arr[tempArr[j]][y]++;
-                            tempArr[j]=-1;
-                        }
-                        break;
-                    case 1:
-                        if(arr[x][tempArr[j]]!==0){
-                            console.log('arr['+x+']['+tempArr[j]+']='+arr[x][tempArr[j]]);
-                            arr[x][tempArr[j]]++;
-                            tempArr[j]=-1;
-                        }
-                        break;
-                    case 2:
-                        if(arr[tempArr[j]][y]!==0){
-                            console.log('arr['+tempArr[j]+']['+y+']='+arr[tempArr[j]][y]);
-                            arr[tempArr[j]][y]++;
-                            tempArr[j]=6;
-                        }
-                        break;
-                    case 3:
-                        if(arr[x][tempArr[j]]!==0){
-                            console.log('arr['+x+']['+tempArr[j]+']='+arr[x][tempArr[j]]);
-                            arr[x][tempArr[j]]++;
-                            tempArr[j]=6;
-                        }
-                        break;
-                }
-            }
-
-        }
-        console.log('===============================');
+function dotclick(arr,x,y) {
+    arr[x][y]++;
+    if(arr[x][y]>=5){
+        arr[x][y]=0;
+        dotboom(arr,x,y);
     }
 
+}
+
+function dotboom(arr,x,y) {
+    for(let i=1;x-i>=0;i++){
+        if(arr[x-i][y]){
+            arr[x-i][y]++;console.log('(x-i,y)=('+x+','+y+')');
+            if(arr[x-i][y]===5){
+                arr[x-i][y]=0;console.log('(x-i,y)=('+(x-i)+','+y+') boom!');
+                dotboom(arr,x-i,y);
+            }
+            break;
+        }
+    }
+    for(let i=1;x+i<=5;i++){
+        if(arr[x+i][y]){
+            arr[x+i][y]++;console.log('(x+i,y)=('+x+','+y+')');
+            if(arr[x+i][y]===5){
+                arr[x+i][y]=0;console.log('(x+i,y)=('+(x+i)+','+y+') boom!');
+                dotboom(arr,x+i,y);
+            }
+            break;
+        }
+    }
+    for(let i=1;y-i>=0;i++){
+        if(arr[x][y-i]){
+            arr[x][y-i]++;console.log('(x,y-i)=('+x+','+y+')');
+            if(arr[x][y-i]===5){
+                arr[x][y-i]=0;console.log('(x,y-i)=('+x+','+(y-i)+') boom!');
+                dotboom(arr,x,y-i);
+            }
+            break;
+        }
+    }
+    for(let i=1;y+i<=5;i++){
+        if(arr[x][y+i]){
+            arr[x][y+i]++;console.log('(x,y+i)=('+x+','+y+')');
+            if(arr[x][y+i]===5){
+                arr[x][y+i]=0;console.log('(x,y+i)=('+x+','+(y+i)+') boom!');
+                dotboom(arr,x,y+i);
+            }
+            break;
+        }
+    }
+    console.log('=========================');
 }
